dba2424a4fc61fe341facc2ec5807b955d5837b8
Lauren Dove <elledoveinc@gmail.com>
2020-10-11 20:45:59 -0400

    Done.

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class About extends Component {
+    
+    render() {
+        return (
+            <h1> This is my about Component</h1>
+        )
+    }
+}
+
+
+
+export default About;
