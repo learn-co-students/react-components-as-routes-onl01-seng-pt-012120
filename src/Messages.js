5d17e6998244ff9e4786d8ebb7256ca1aa4c9ef3
DCTorres87 <cruz_danielle_torres@yahoo.com>
2020-10-16 15:51:58 -0400

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,10 @@
+// src/Messages.js
+import React from 'react';
+ 
+class Messages extends React.Component {
+  render() {
+    return <h1>Please leave a message!</h1>
+  }
+}
+ 
+export default Messages;
